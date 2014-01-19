cf1e2ade36462232d7d0de0d61c40285bcb3b842
Tim Willis <tim@willishq.com>
2014-01-18 21:56:56 -0500

    Update this stupid thing.

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template, redirect, url_for
-from sh import totem
+from flask import Flask, render_template, redirect, url_for, flash
+from sh import totem, ErrorReturnCode
+
+from contextlib import contextmanager
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'cheese'
+
+
+@contextmanager
+def hide_errors():
+    try:
+        yield
+    except ErrorReturnCode:
+        flash('Oops, I screwed something up.')        
+
 
 @app.route('/')
 def index():
@@ -10,32 +24,38 @@ def index():
 @app.route('/play/')
 @app.route('/pause/')
 def play_pause():
-    totem("--play-pause") 
+    with hide_errors():
+        totem("--play-pause") 
     return redirect(url_for('index'))
 
 @app.route('/volume/up/')
 def volume_up():
-    totem("--volume-up")
+    with hide_errors():
+    	totem("--volume-up")
     return redirect(url_for('index'))
 
 @app.route('/volume/down/')
 def volume_down():
-    totem("--volume-down")
+    with hide_errors():
+        totem("--volume-down")
     return redirect(url_for('index'))
 
 @app.route('/fullscreen/')
 def fullscreen():
-    totem('--fullscreen')
+    with hide_errors():
+        totem('--fullscreen')
     return redirect(url_for('index'))
 
 @app.route('/seek/backward/')
 def backward():
-    totem('--seek-bwd')
+    with hide_errors():
+        totem('--seek-bwd')
     return redirect(url_for('index'))
 
 @app.route('/seek/foreward/')
 def forward():
-    totem('--seek-fwd')
+    with hide_errors():
+        totem('--seek-fwd')
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
